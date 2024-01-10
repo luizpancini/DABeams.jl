9440e684bc984d2b6f8d6ac076ba600bbdc8f51f
luizpancini <luizpancini@usp.br>
2024-01-10 19:41:40 -0300

    Generate docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,15 @@
-# Inside make.jl
-push!(LOAD_PATH,"../src/")
-using DABeams
 using Documenter
+using DABeams
+
 makedocs(
-         sitename = "DABeams.jl",
-         modules  = [DABeams],
-         pages=[
-                "Home" => "index.md"
-               ])
-deploydocs(;
-    repo="github.com/luizpancini/DABeams.jl",
+    sitename = "DABeams",
+    format = Documenter.HTML(),
+    modules = [DABeams]
+)
+
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
+deploydocs(
+    repo = "https://github.com/luizpancini/DABeams.jl"
 )
