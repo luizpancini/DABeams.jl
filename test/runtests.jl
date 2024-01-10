b069d51a75a42a6012f2d253329315f05b2514e3
luizpancini <luizpancini@usp.br>
2024-01-10 13:53:11 -0300

    add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,6 @@ using Test
 @testset "DABeamS.jl" begin
     @test DABeamS.greeting() == "You are using the DABeamS package"   
     @test DABeamS.plusTwo!(3) == 5
-    @test DABeamS.plusTwo!(3.0) == 5.0    
+    @test DABeamS.plusTwo!(3.0) == 5.0 
+    @test DABeamS.plusTwo!(1.0) == 3.0   
 end
